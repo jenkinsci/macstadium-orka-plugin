ea7afe134307fcbb397555c203273182f3455f4e
Ivan Spasov <frankeys89@gmail.com>
2019-08-07 11:28:01 +0300

    Add permission check when working with credentials

--- src/main/java/io/jenkins/plugins/orka/helpers/CredentialsHelper.java
+++ src/main/java/io/jenkins/plugins/orka/helpers/CredentialsHelper.java
@@ -4,18 +4,19 @@ import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
-
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
 
 public final class CredentialsHelper {
     public static StandardUsernamePasswordCredentials lookupSystemCredentials(final String credentialsId) {
+        Jenkins.getInstance().checkPermission(Jenkins.ADMINISTER);
         return CredentialsMatchers.firstOrNull(
                 CredentialsProvider.lookupCredentials(StandardUsernamePasswordCredentials.class, Jenkins.getInstance()),
                 CredentialsMatchers.withId(credentialsId));
     }
 
     public static ListBoxModel getCredentials(Class type) {
+        Jenkins.getInstance().checkPermission(Jenkins.ADMINISTER);
         return new StandardListBoxModel().includeEmptyValue().withMatching(CredentialsMatchers.always(),
                 CredentialsProvider.lookupCredentials(type, Jenkins.getInstance()));
     }
