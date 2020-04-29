89dd97db9822147f34969d7240c210cda1db5c23
Dimitar Tsonev <dimitar@st6.io>
2020-04-29 16:24:04 +0300

    Extract OrkaClient creation

--- src/main/java/io/jenkins/plugins/orka/helpers/OrkaClientProxy.java
+++ src/main/java/io/jenkins/plugins/orka/helpers/OrkaClientProxy.java
@@ -31,22 +31,19 @@ public class OrkaClientProxy {
     }
 
     public List<VMResponse> getVMs() throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.getVMs();
         }
     }
 
     public List<NodeResponse> getNodes() throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.getNodes();
         }
     }
 
     public List<String> getImages() throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.getImages();
         }
     }
@@ -54,37 +51,37 @@ public class OrkaClientProxy {
     public ConfigurationResponse createConfiguration(String vmName, String image, String baseImage,
             String configTemplate, int cpuCount) throws IOException {
 
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.createConfiguration(vmName, image, baseImage, configTemplate, cpuCount);
         }
     }
 
     public DeploymentResponse deployVM(String vmName) throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.deployVM(vmName);
         }
     }
 
     public DeploymentResponse deployVM(String vmName, String node) throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.deployVM(vmName, node);
         }
     }
 
     public DeletionResponse deleteVM(String vmName) throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.deleteVM(vmName);
         }
     }
 
     public DeletionResponse deleteVM(String vmName, String node) throws IOException {
-        try (OrkaClient client = new OrkaClient(endpoint, credentials.getUsername(),
-                Secret.toString(credentials.getPassword()))) {
+        try (OrkaClient client = getOrkaClient()) {
             return client.deleteVM(vmName, node);
         }
+    }
+
+    private OrkaClient getOrkaClient() throws IOException {
+        return new OrkaClient(this.endpoint, this.credentials.getUsername(),
+                Secret.toString(credentials.getPassword()));
     }
 }
