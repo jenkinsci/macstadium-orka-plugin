5097cb7eef6fd0eac554473451fa5056be31d627
Veselina Radeva <vesita@gmail.com>
2020-10-28 22:06:27 +0200

    extend OrkaCloud constructor with http proxy value

--- integration-tests/jenkins-setup/init-cloud.groovy
+++ integration-tests/jenkins-setup/init-cloud.groovy
@@ -48,7 +48,7 @@ AgentTemplate template = new AgentTemplate(sshUserCredentialsId, vmConfigName, f
 ArrayList<AgentTemplate> templates = new ArrayList<AgentTemplate>()
 templates.add(template)
 
-OrkaCloud cloud = new OrkaCloud(cloudConfigName, orkaUserCredentialsId, orkaEndpoint, "1", 300, null, templates)
+OrkaCloud cloud = new OrkaCloud(cloudConfigName, orkaUserCredentialsId, orkaEndpoint, "1", 300, false, null, templates)
 
 Jenkins.instance.clouds.add(cloud)
 
