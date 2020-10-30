cc253d75c5db8a5d12c72ac7e1f38c2afa366474
Veselina Radeva <vesita@gmail.com>
2020-10-30 14:55:33 +0200

    get full test name + small refactoring

--- integration-tests/CustomNodeEnvironment.js
+++ integration-tests/CustomNodeEnvironment.js
@@ -5,25 +5,40 @@ class CustomNodeEnvironment extends NodeEnvironment {
         super(config);
     }
 
-    async handleTestEvent(event, state) {
-        if (event.name === 'test_start') {
-            console.log(
-                `\x1b[45mSTARTED\x1b[0m ${event.test.parent.name} >> ${event.test.name}`,
-            );
+    getTestName(test) {
+        if (!test || !test.name) {
+            return '';
+        }
+        let testName = test.name;
+        let parent = test.parent;
+        while (parent) {
+            if (parent.name === 'ROOT_DESCRIBE_BLOCK') {
+                break;
+            }
+            testName = `${parent.name} >> ${testName}`;
+            parent = parent.parent;
         }
 
-        if (event.name === 'test_done') {
-            const testName = `${event.test.parent.name} >> ${event.test.name}`;
+        return testName;
+    }
 
-            if (event.test.errors.length > 0) {
-                console.log(`\x1b[41mFAILED\x1b[0m ${testName}`);
+    async handleTestEvent(event, state) {
+        const testName = this.getTestName(event.test);
+        switch (event.name) {
+            case 'test_start':
+                console.log(`\x1b[45mSTARTED\x1b[0m ${testName}`);
+                break;
+            case 'test_done':
+                if (event.test.errors.length > 0) {
+                    console.log(`\x1b[41mFAILED\x1b[0m ${testName}`);
 
-                for (const e of event.test.errors) {
-                    console.log(e[0].stack);
+                    for (const e of event.test.errors) {
+                        console.log(e[0].stack);
+                    }
+                } else {
+                    console.log(`\x1b[42mPASSED\x1b[0m ${testName}`);
                 }
-            } else {
-                console.log(`\x1b[42mPASSED\x1b[0m ${testName}`);
-            }
+                break;
         }
     }
 }
