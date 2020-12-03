64df7abbf752cb73ffa5aaad64d8cf999a3f927b
Ben Fletcher <bfletcher@macstadium.com>
2020-12-03 09:49:17 -0500

    Remove extraneous whitespace

--- src/main/java/io/jenkins/plugins/orka/RunOnceCloudRetentionStrategy.java
+++ src/main/java/io/jenkins/plugins/orka/RunOnceCloudRetentionStrategy.java
@@ -31,7 +31,7 @@ public class RunOnceCloudRetentionStrategy extends CloudRetentionStrategy implem
         
         this.idleMinutes = Utils.normalizeIdleTime(idleMinutes, RECOMMENDED_MIN_IDLE);
     }
-    
+
     public int getIdleMinutes() {
         return idleMinutes;
     }
