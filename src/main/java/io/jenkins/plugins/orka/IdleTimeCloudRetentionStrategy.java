a05389456f8433c71e1f2bf488425670507add54
Ben Fletcher <bfletch@gmail.com>
2020-12-07 07:51:07 -0500

    Update Idle Timeout validation checks (#39)
    Ensure valid values for timeouts are enforced along with helpful user messaging.

--- src/main/java/io/jenkins/plugins/orka/IdleTimeCloudRetentionStrategy.java
+++ src/main/java/io/jenkins/plugins/orka/IdleTimeCloudRetentionStrategy.java
@@ -4,16 +4,22 @@ import hudson.Extension;
 import hudson.model.Descriptor;
 import hudson.slaves.CloudRetentionStrategy;
 import hudson.slaves.RetentionStrategy;
+import hudson.util.FormValidation;
+import io.jenkins.plugins.orka.helpers.Utils;
 
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+
 
 public class IdleTimeCloudRetentionStrategy extends CloudRetentionStrategy {
-    private final int idleMinutes;
-
+    private int idleMinutes;
+    public static final int RECOMMENDED_MIN_IDLE = 30;
+    
     @DataBoundConstructor
     public IdleTimeCloudRetentionStrategy(int idleMinutes) {
-        super(idleMinutes);
-        this.idleMinutes = idleMinutes;
+        super(Utils.normalizeIdleTime(idleMinutes, RECOMMENDED_MIN_IDLE));
+        
+        this.idleMinutes = Utils.normalizeIdleTime(idleMinutes, RECOMMENDED_MIN_IDLE);
     }
 
     public int getIdleMinutes() {
@@ -33,9 +39,15 @@ public class IdleTimeCloudRetentionStrategy extends CloudRetentionStrategy {
         public String getDisplayName() {
             return "Keep until idle time expires";
         }
+        
+        public FormValidation doCheckIdleMinutes(@QueryParameter String value) {
+            return Utils.checkInputValue(value);
+        }
     }
 
     private Object readResolve() {
+        this.idleMinutes = Utils.normalizeIdleTime(this.idleMinutes, RECOMMENDED_MIN_IDLE);
+
         return new IdleTimeCloudRetentionStrategy(idleMinutes);
     }
 }
