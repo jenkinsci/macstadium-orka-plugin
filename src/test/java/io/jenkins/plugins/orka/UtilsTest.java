a225c14ad39fe7f34bf1e5b7671caab48d06e5e4
Ivan Spasov <ispasov@users.noreply.github.com>
2023-09-19 12:15:34 +0300

    Ensure we do not override the agent config base image (#114)
    In case the base image is removed or renamed we will silently change the image without telling the user.
    This can lead to undersired behavior and it will be hard to troubleshoot

--- src/test/java/io/jenkins/plugins/orka/UtilsTest.java
+++ src/test/java/io/jenkins/plugins/orka/UtilsTest.java
@@ -1,13 +1,15 @@
 package io.jenkins.plugins.orka;
 
 import io.jenkins.plugins.orka.helpers.Utils;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class UtilsTest {
     @Test
-    public void whrn_comparing_versions_should_return_correct_result() {
+    public void when_comparing_versions_should_return_correct_result() {
         assertTrue(Utils.compareVersions("1.0.0", "1.1.2") < 0);
         assertTrue(Utils.compareVersions("1.0.1", "1.10") < 0);
         assertTrue(Utils.compareVersions("1.1.2", "1.0.1") > 0);
@@ -18,5 +20,15 @@ public class UtilsTest {
         assertTrue(Utils.compareVersions("2.1.1-preview-1", "2.1.1-preview-2") == 0);
         assertTrue(Utils.compareVersions("2.1.1", "2.0") > 0);
         assertTrue(Utils.compareVersions("2.1.1", "2.1.1") == 0);
+    }
+
+    @Test
+    public void when_sanitizing_name_should_return_correct_name() {
+        assertEquals(Utils.sanitizeK8sName("Mojave"), "mojave");
+        assertEquals(Utils.sanitizeK8sName("90GBCatalina"), "90gbcatalina");
+        assertEquals(Utils.sanitizeK8sName("ventura"), "ventura");
+        assertEquals(Utils.sanitizeK8sName("ventura-new"), "ventura-new");
+        assertEquals(Utils.sanitizeK8sName("ventura_invalid"), "ventura-invalid");
+        assertEquals(Utils.sanitizeK8sName("Mojave_Invalid"), "mojave-invalid");
     }
 }
