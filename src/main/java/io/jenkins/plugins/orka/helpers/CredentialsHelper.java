8099cd4e1e4ef0b8eb5b6a66993853960c23c14c
Ivan Spasov <ispasov@users.noreply.github.com>
2024-05-22 13:59:57 +0300

    Allow using SecretText for storing tokens (#118)
    Currently, we only allow Username/Password credentials.
    However, this is counter-intuitive as we want users to only store tokens.
    Allow SecretText as well, which is intended for tokens

--- src/main/java/io/jenkins/plugins/orka/helpers/CredentialsHelper.java
+++ src/main/java/io/jenkins/plugins/orka/helpers/CredentialsHelper.java
@@ -3,14 +3,16 @@ package io.jenkins.plugins.orka.helpers;
 import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsMatchers;
 import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.PasswordCredentials;
 import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 
 import hudson.security.ACL;
 import hudson.util.ListBoxModel;
-
+import hudson.util.Secret;
 import java.util.Collections;
 import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 
 public final class CredentialsHelper {
     public static StandardUsernamePasswordCredentials lookupSystemCredentials(final String credentialsId) {
@@ -23,8 +25,26 @@ public final class CredentialsHelper {
                 CredentialsMatchers.withId(credentialsId));
     }
 
+    public static StringCredentials lookupStringCredentials(final String credentialsId) {
+        return lookupSystemCredentials(credentialsId, StringCredentials.class);
+    }
+
+    public static Secret lookupTokenSecret(final String credentialsId) {
+        StringCredentials stringCredential = CredentialsHelper.lookupStringCredentials(credentialsId);
+        if (stringCredential != null) {
+            return stringCredential.getSecret();
+        }
+
+        PasswordCredentials passwordCredentials = CredentialsHelper.lookupSystemCredentials(credentialsId);
+        if (passwordCredentials != null) {
+            return passwordCredentials.getPassword();
+        }
+        return null;
+    }
+
     public static ListBoxModel getCredentials(Class type) {
         return new StandardListBoxModel().includeEmptyValue().includeMatchingAs(ACL.SYSTEM, Jenkins.get(), type,
                 Collections.emptyList(), CredentialsMatchers.always());
     }
+
 }
--- src/test/java/io/jenkins/plugins/orka/OrkaAgentDescriptorImplTest.java
+++ src/test/java/io/jenkins/plugins/orka/OrkaAgentDescriptorImplTest.java
@@ -8,13 +8,15 @@ import com.cloudbees.plugins.credentials.CredentialsScope;
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl;
 
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
-import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
+import hudson.util.Secret;
 
 public class OrkaAgentDescriptorImplTest {
     @ClassRule
@@ -48,6 +50,27 @@ public class OrkaAgentDescriptorImplTest {
         assertEquals(2, model.size());
         assertEquals("", model.get(0).value);
         assertEquals(credentials.getId(), model.get(1).value);
+    }
+
+    @Test
+    public void when_do_fill_orka_credentials_id_items_with_different_credential_should_return_three_credentials()
+            throws IOException {
+        OrkaAgent.DescriptorImpl descriptor = new OrkaAgent.DescriptorImpl();
+        UsernamePasswordCredentialsImpl credentials = new UsernamePasswordCredentialsImpl(CredentialsScope.SYSTEM,
+                "uniqueId", "description", "foo", "bar");
+
+        StringCredentials stringCredentials = new StringCredentialsImpl(
+                CredentialsScope.SYSTEM, "stringId", "description", Secret.fromString("someSecret"));
+        SystemCredentialsProvider.getInstance().getCredentials().clear();
+        SystemCredentialsProvider.getInstance().getCredentials().add(credentials);
+        SystemCredentialsProvider.getInstance().getCredentials().add(stringCredentials);
+        SystemCredentialsProvider.getInstance().save();
+
+        ListBoxModel model = descriptor.doFillOrkaCredentialsIdItems();
+        assertEquals(3, model.size());
+        assertEquals("", model.get(0).value);
+        assertEquals(credentials.getId(), model.get(2).value);
+        assertEquals(stringCredentials.getId(), model.get(1).value);
     }
 
     @Test
