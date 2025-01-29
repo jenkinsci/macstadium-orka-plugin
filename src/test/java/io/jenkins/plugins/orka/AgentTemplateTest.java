ff937a0b40970dbf670eaddd4c3e7a8757c1646b
Bruno Verachten <gounthar@users.noreply.github.com>
2025-01-29 21:11:20 +0100

    Require 2.346.3

--- src/test/java/io/jenkins/plugins/orka/AgentTemplateTest.java
+++ src/test/java/io/jenkins/plugins/orka/AgentTemplateTest.java
@@ -2,7 +2,6 @@ package io.jenkins.plugins.orka;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
